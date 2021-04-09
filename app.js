e96548fb547e51c179794024fb2647c03d1e3c5d
nmparfait <mbamu.developer@gmail.com>
2021-04-09 09:40:03 -0400

    close the btn and the modal

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ function Gallery(element) {
   this.prevBtn = getElement('.prev-btn');
   //self reference
   //let self = this;
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
 
   //bind functions
   //this.openModal = this.openModal.bind(this);
@@ -47,12 +50,25 @@ Gallery.prototype.openModal = function (selectedImage, list) {
     .join('');
   this.setMainImage(selectedImage);
   this.modal.classList.add('open');
+  this.closeBtn.addEventListener('click', this.closeModal);
+  this.nextBtn.addEventListener('click', this.nextImage);
+  this.prevBtn.addEventListener('click', this.prevImage);
 };
 
 Gallery.prototype.setMainImage = function (selectedImage) {
   this.modalImg.src = selectedImage.src;
   this.imageName.textContent = selectedImage.title;
 };
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener('click', this.closeModal);
+  this.nextBtn.removeventListener('click', this.nextImage);
+  this.prevBtn.removeventListener('click', this.prevImage);
+};
+
+Gallery.prototype.nextImage = function () {};
+Gallery.prototype.prevImage = function () {};
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
