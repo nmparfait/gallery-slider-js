775291e717febf7d39a8ac32782b954907e47115
nmparfait <mbamu.developer@gmail.com>
2021-04-09 09:25:56 -0400

    open modal displaying the name, added one more city image

--- app.js
+++ app.js
@@ -9,16 +9,50 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll('.img')];
   //target
   this.modal = getElement('.modal');
+  this.imageName = getElement('.image-name');
   this.modalImg = getElement('.main-img');
   this.modalImages = getElement('.modal-images');
   this.closeBtn = getElement('.close-btn');
-  this.prevBtn = getElement('.prev-btn');
   this.nextBtn = getElement('.next-btn');
-  //console.log(this.list);
+  this.prevBtn = getElement('.prev-btn');
+  //self reference
+  //let self = this;
+
+  //bind functions
+  //this.openModal = this.openModal.bind(this);
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target, this.list);
+      }
+      // self.openModal();
+    }.bind(this)
+  );
 }
+
+Gallery.prototype.openModal = function (selectedImage, list) {
+  //console.log(this);
+  //console.log(selectedImage, list);
+  this.modalImages.innerHTML = list
+    .map(function (image) {
+      return `<img src="${
+        image.src
+      }" title = "${image.title} data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"/>`;
+    })
+    .join('');
+  this.setMainImage(selectedImage);
+  this.modal.classList.add('open');
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
+};
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
